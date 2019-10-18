33867eeaa40a50109b7ee263d2d6783c9f5b0a9d
fernando <jferbarrera@gmail.com>
2019-10-18 14:58:40 -0500

    added online profile

---
+++ main/addPathToLibraries.m
@@ -0,0 +1,15 @@
+function addPathToLibraries()
+
+fprintf('***************************\n');
+fprintf('** assistiveTech toolbox **\n');
+fprintf('***************************\n\n');
+
+% paths to local functions
+addpath( '../core/' );
+addpath( '../main/');
+addpath( '../utils/');
+addpath( '../dataset/cameraParameters/');
+
+fprintf('\n');
+fprintf('\nLoading toolbox dependencies .... OK\n');
+end
--- main/pc_visualization_online.m
+++ main/pc_visualization_online.m
@@ -1,55 +1,145 @@
-% ---------------------------------------------
-% - Point Cloud extraction and visualization
-% ---------------------------------------------
+% ----------------------------------------------
+% - Save color and depth images 
+% ----------------------------------------------
 
-% init color and depth camera
-colorDevice = imaq.VideoDevice('kinect',1);
-depthDevice = imaq.VideoDevice('kinect',2);
+clear all;
+close all;
 
-% leer camera parameters
-k_file = exist(strcat( intrinsic_calib_path ,'k_test.mat'),'file') == 2;
-dist_file = exist(strcat( intrinsic_calib_path ,'dist_test.mat'),'file') == 2;
+%% parameters
 
-if ( k_file && dist_file )
-    load( strcat( intrinsic_calib_path ,'k_test.mat') );
-    load( strcat( intrinsic_calib_path ,'dist_test.mat') );
-else
-    K = [528 0 320; 0 528 240; 0 0 1];
-    dist = zeros(1,5);
-    sprintf('working with default parameters');
-end
+camera_k_mat = 'kinect_v2_color_k_2019';        % kinectV2_ref 
+camera_dist_mat = 'kinect_v2_color_dist_2019';  % kinectV2_ref 
+im_counter = 0;
 
-%Inicia las camaras
-step(colorDevice);
-step(depthDevice);
-  
-%Graba dispositivos
-colorImage = step(colorDevice);
-depthImage = step(depthDevice);
-  
-%Extrae nube de puntos.
-ptCloud = pcfromkinect(depthDevice, depthImage, colorImage);
+% point cloud limits
+xlimits = [-2500 2500];
+ylimits = [-2500 2500];
+zlimits = [0 6000];
 
-% Inicializar un reproductor para visualizar datos de nube de puntos tridimensionales. El eje es
-% Establecido correctamente para visualizar la nube de puntos de Kinect.
-player = pcplayer(ptCloud.XLimits, ptCloud.YLimits, ptCloud.ZLimits,...
-               'VerticalAxis', 'y', 'VerticalAxisDir', 'down');
+% plane detection algorithm
+maxDistance = 150;
+referenceNormal = [0,1,0.1];
+maxAngularDistance = 10;
 
-xlabel(player.Axes, 'X (m)');
-ylabel(player.Axes, 'Y (m)');
-zlabel(player.Axes, 'Z (m)');
+% voxel size
+voxel_size = [50 50 50];
+voxel_number = [80 1 100];
 
-% Adquirir y ver nube de puntos del kinect.
-while isOpen(player)
-    colorImage = step(colorDevice);
-    depthImage = step(depthDevice);
- 
-    ptCloud = pcfromkinect(depthDevice, depthImage, colorImage);
- 
+%%
+
+% load camera parameters
+[ K ] = loadCameraIntrinsic( camera_k_mat );
+[ dist ] = loadCameraDistortion( camera_dist_mat );
+
+% player setup 
+player = pcplayer(xlimits,ylimits,zlimits,'VerticalAxis','Y','VerticalAxisDir','Down');
+xlabel(player.Axes,'X (mm)');
+ylabel(player.Axes,'Y (mm)');
+zlabel(player.Axes,'Z (mm)');
+hold(player.Axes,'on');
+
+%view
+%camtarget(player.Axes,[0 0 2000]);
+%campos(player.Axes,[0 -2000 -2000]);
+
+%view
+campos(player.Axes,[31.19 -42.38 -5.75]);
+camtarget(player.Axes,[0 0 3025]);
+%view(player.Axes,52.35,-75.25);
+
+%view
+%campos(player.Axes,[2800 -1550 1930]);
+%camtarget(player.Axes,[0 0 3525]);
+
+% 2D 
+figure('Name', '3D to 2D mapping');
+mapping2Daxe = gca;
+init3dto2dFloorMap( mapping2Daxe, voxel_number, voxel_size);
+
+% init camera
+info =  imaqhwinfo('kinect');   % informaci?n del sensor
+info.DeviceInfo(1)              % acceso a la c?mara
+s = 0;
+
+colorVid = videoinput('kinect',1,'BGR_1920x1080'); %activa c?mara RGB video
+depthVid = videoinput('kinect',2,'Depth_512x424'); %activa c?mara depth video
+    
+% Set Frames per trigger. Bigger number means more frames when triggered.
+colorVid.FramesPerTrigger = 1;
+depthVid.FramesPerTrigger = 1;
+
+% Set the trigger repeat for both devices to Inf
+colorVid.TriggerRepeat = Inf;
+depthVid.TriggerRepeat = Inf;
+
+% Set triggers to manual so it is possible to work with them
+triggerconfig([colorVid depthVid],'manual');
+
+% Start both video objects
+start([colorVid depthVid]);
+
+%% recording loop
+
+while s>=0
+
+    % Trigger both objects.
+    trigger([colorVid depthVid]);
+    
+     % Get the acquired frames and metadata.
+    [color, ts_color, metaData_Color] = getdata(colorVid);
+    [depth, ts_depth, metaData_Depth] = getdata(depthVid);
+
+    %scale depth image
+    [depth] = scaleImage(depth, size(color,1), size(color,2));
+    
+    % getting 3d information
+    [ X, X_valid, valid_depth_mask ] = anav_getXfromDepthmap( depth, K, dist );
+    
+    % getting colors
+    C = reshape(color, numel(color)/3, 3);
+    C_valid = C(valid_depth_mask,:);
+    
+    % colored point cloud
+    ptCloud = pointCloud( X_valid(1:3,:)', 'Color', C_valid );
+    
+    % compute floor plane
+    [ floor_normal, floor_centroid, plane_model, inlierIndices, outlierIndices ] = getFloorPlane( ptCloud, ...
+        maxDistance, referenceNormal, maxAngularDistance);
+    
+    % highlight detected floor
+    if (inlierIndices > 0)
+        ptCloud.Color(inlierIndices,3) = 255;
+    end
+    
+    % generate floor rotation (or floor coordinate system)
+    [ plane_normal, d_sign ] = anav_flipNormalTowardCamera( floor_normal, floor_centroid );
+    [ plane_R ] = anav_generateRotation( plane_normal );
+        
+    % plot mapping 3d-to-2d
+    [ map , grid ] = anav_generateMapping( plane_model, floor_centroid, X_valid(:,outlierIndices), voxel_number, voxel_size);
+    imagesc( mapping2Daxe, 'CData', map );
+    drawnow;
+    
+    % viewer
     view(player, ptCloud);
+    delete previous plots
+    plot_handles = get(player.Axes,'Children');
+    if (numel(plot_handles)>1)
+        delete(plot_handles(1:end-1));
+    end
+    
+    % add new plots
+    plotAxe( player.Axes, plane_R, floor_centroid ); % draw plane axe
+    
+    ind_first_layer=(voxel_number(1)+1)*(voxel_number(3)+1);
+    scatter3(player.Axes,grid(1:16:ind_first_layer,1),grid(1:16:ind_first_layer,2),grid(1:16:ind_first_layer,3),'MarkerEdgeColor','k','MarkerFaceColor',[.9 0.1 .1]);
+    
+    drawnow;
+    
+    s = s+1;
+
 end
-   
-% release memory object
-release(colorDevice);
-release(depthDevice);
+
+delete(colorVid);
+delete(depthVid);
   
